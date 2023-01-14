5e215575323cd472db4748cb7e47c6c82cf10559
Kacper Gajko <kacper.gajko1@gmail.com>
2023-01-14 15:36:56 +0100

    Working App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [Silver, SetSilver] = useState(0);
+  const [Players, SetPlayers] = useState(1);
+  const [Repair, SetRepair] = useState(0);
+  const [FinalSilver, SetFinalSilver] = useState(0);
+
+  const handleSilver = (event) => {
+    SetSilver(event.target.value);
+  }
+
+  const handlePlayers = (event) => {
+    SetPlayers(event.target.value)
+  }
+
+  const handleRepair = (event) => {
+    SetRepair(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    SetFinalSilver((Silver - Repair) / Players);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div'>
+      <form onSubmit={handleSubmit}>
+        Total Silver: <br/>
+        <input 
+          id="Silver"
+          type="number"  
+          value={Silver}
+          onChange={handleSilver}/><br/>
+        Number of players: <br/>
+        <input
+          id="players"
+          type="number"
+          value={Players}
+          onChange={handlePlayers}/><br/>
+        Repair cost: <br/>
+        <input
+          id="Repair"
+          type="number"
+          value={Repair}
+          onChange={handleRepair}/><br/>
+        <input 
+          id="Submit"
+          type="submit" 
+          value="Calculate" />
+      </form>
+      <p>{FinalSilver}</p>
     </div>
   );
 }
